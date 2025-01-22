9060c8d36f2d88c090edf24370087187878b976c
wuwenlong <773314621@qq.com>
2025-01-22 22:54:31 +0800

    bugfix

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -62,7 +62,7 @@ const Modal: React.FC<ModalProps> = ({
 
   return (
     <AnimatePresence>
-      {isShowModal && (
+      {isShowModal ? (
         <motion.div
           className={styles.container}
           style={
@@ -116,7 +116,7 @@ const Modal: React.FC<ModalProps> = ({
             {children}
           </div>
         </motion.div>
-      )}
+      ) : null}
     </AnimatePresence>
   );
 };
